0e84f4ef950b10aabd95488e27b5f5c5fe670713
Steve Huff <shuff@vecna.org>
2017-06-27 14:12:24 -0400

    adding iam management

---
+++ iam.tf
@@ -0,0 +1,64 @@
+data "aws_iam_policy_document" "pganalyze_task_policy" {
+  statement {
+    actions = [
+      "ec2:Describe*",
+      "autoscaling:Describe*",
+      "ec2:DescribeAddresses",
+      "ec2:DescribeInstances",
+      "ec2:DescribeTags",
+    ]
+
+    resources = ["*"]
+  }
+}
+
+data "aws_iam_policy_document" "assume_role_pganalyze_task" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "pganalyze_task" {
+  name               = "tf-${data.aws_vpc.vpc.tags["Name"]}-pganalyze-ecsTaskRole"
+  path               = "/"
+  assume_role_policy = "${data.aws_iam_policy_document.assume_role_pganalyze_task.json}"
+}
+
+resource "aws_iam_role_policy" "pganalyze_ecs_task" {
+  name   = "tf-${data.aws_vpc.vpc.tags["Name"]}-pganalyze-ecsTaskPolicy"
+  role   = "${aws_iam_role.pganalyze_task.id}"
+  policy = "${data.aws_iam_policy_document.pganalyze_task_policy.json}"
+}
+
+# ecsServiceRole for pganalyze
+
+resource "aws_iam_role" "ecsServiceRole" {
+  name = "tf-${data.aws_vpc.vpc.tags["Name"]}-pganalyze-ecsServiceRole"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+      "Service": ["ecs.amazonaws.com"]
+
+    },
+    "Effect": "Allow",
+    "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "attach-ecsServiceRole" {
+  role       = "${aws_iam_role.ecsServiceRole.name}"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceRole"
+}
